33a87aeae960801a624ed7a521a8557eb5b4ed85
rahul <77121568+RahulTambatUbz@users.noreply.github.com>
2024-09-12 18:29:18 +0530

    new env

--- Assets/Scripts/DestroyPlayer.cs
+++ Assets/Scripts/DestroyPlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DestroyPlayer : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    public  void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
